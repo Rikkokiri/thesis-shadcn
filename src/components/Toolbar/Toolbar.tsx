429498c7e708ac961070085e708106d58458c8da
Ossian Rajala <ossian.rajala@gmail.com>
2024-06-25 16:27:06 +0300

    fix: toolbar sizing, run updated screenshot tests (almost perfect matches)

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -7,8 +7,8 @@ export const Toolbar = () => {
   const { t } = useTranslation();
 
   return (
-    <header className="h-toolbar bg-background relative w-full h-[72px] flex flex-row items-center px-6 shadow-toolbar">
-      <nav className="toolbar__nav font-bold flex flex-row justify-center items-center max-w-[1080px]">
+    <header className="h-toolbar bg-background relative w-full h-[72px] flex flex-row items-center justify-center px-6 shadow-toolbar">
+      <nav className="toolbar__nav font-bold flex flex-row justify-start items-center max-w-[1080px] w-full">
         <div className="flex flex-row items-center gap-6">
           <div className="toolbar__logo size-10 bg-logo rounded-sm"></div>
           <Link to={Route.ROOT} className="no-underline">
